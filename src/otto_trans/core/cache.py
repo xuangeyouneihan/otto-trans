d8e2675b24608b0f83971a4b7602e24b70a0cba5
xuangeyouneihan <me@xuan.us.kg>
2026-05-19 23:24:53 +0800

    完善缓存部分

---
+++ src/otto_trans/core/cache.py
@@ -0,0 +1,51 @@
+import sqlite3
+import base64
+import atexit
+
+def b64encode(s: str) -> str:
+    return base64.b64encode(s.encode(encoding="utf-8")).decode(encoding="utf-8")
+
+def b64decode(s: str) -> str:
+    return base64.b64decode(s.encode(encoding="utf-8")).decode(encoding="utf-8")
+
+class Cache:
+    _instance = None
+    _initialized = False
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, db_path: str):
+        if self._initialized:
+            return
+        self.db_path = db_path
+        self._conn = sqlite3.connect(db_path)
+        self._initialized = True
+        atexit.register(self._conn.close)
+
+    def __del__(self):
+        self._conn.close()
+
+    def query(self, key: str, engine: str) -> str | None:
+        cursor = self._conn.cursor()
+        cursor.execute(f"SELECT target FROM [{b64encode(engine)}] WHERE source = ?", (b64encode(key),))
+        result = cursor.fetchone()
+        return b64decode(result[0]) if result else None
+
+    def insert(self, key: str, value: str, engine: str):
+        cursor = self._conn.cursor()
+        self._create_table(b64encode(engine))
+        cursor.execute(f"INSERT OR REPLACE INTO [{b64encode(engine)}] (source, target) VALUES (?, ?)", (b64encode(key), b64encode(value)))
+        self._conn.commit()
+
+    def _create_table(self, name: str):
+        cursor = self._conn.cursor()
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS [{name}] (
+                source TEXT PRIMARY KEY,
+                target TEXT
+            )
+        """)
+        self._conn.commit()
